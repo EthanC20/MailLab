5dbfbddc6b5b7dee95b6761b73e91c358bb1f64c
ethan <943192544@qq.com>
2024-06-24 19:45:34 +0800

    SMTP send function finished!

--- send.c
+++ send.c
@@ -13,6 +13,65 @@
 
 char buf[MAX_SIZE+1];
 
+// 为了方便后面的信息发送等操作
+// 以下封装一些socket的函数
+
+#define swap16(x) ((((x)&0xFF) << 8) | (((x) >> 8) & 0xFF))
+
+int custom_send(int sockfd, void *buf, int len, int flag, char *error_msg){
+    int send_len = send(sockfd, buf, len, flag);
+    if (send_len == -1){
+        perror(error_msg);
+        exit(EXIT_FAILURE);
+    }
+    printf("%s\r\n", (char *)buf);
+
+    return send_len;
+}
+
+
+int custom_recv(int sockfd, void *buf, int len, int flag, char *error_msg){
+    int recv_len = recv(sockfd, buf, len, 0);
+    if (recv_len == -1){
+        perror(error_msg);
+        exit(EXIT_FAILURE);
+    }
+    char *temp_buf = (char *)buf;
+    temp_buf[recv_len] = '\0';
+    printf("%s\r\n", temp_buf);
+
+    return recv_len;
+}
+
+
+char *file2str(const char *path){
+    FILE *fp = fopen(path, "r");
+    fseek(fp, 0, SEEK_END);
+    int fplen = ftell(fp);
+    fseek(fp, 0, SEEK_SET);
+    char *content = (char *)malloc(fplen);
+    fread(content, 1, fplen, fp);
+    fclose(fp);
+    return content;
+}
+
+
+int gen_base64_file(const char *path){
+    FILE *fp = fopen(path, "r");
+    if (fp == NULL){
+        perror("fopen");
+        exit(EXIT_FAILURE);
+        return -1;
+    }
+
+    FILE *fp_base64 = fopen("attach_base64", "w");
+    encode_file(fp, fp_base64);
+    fclose(fp);
+    fclose(fp_base64);
+    return 0;
+}
+
+
 // receiver: mail address of the recipient
 // subject: mail subject
 // msg: content of mail body or path to the file containing mail body
@@ -33,8 +92,7 @@ void send_mail(const char* receiver, const char* subject, const char* msg, const
     int r_size;
 
     // Get IP from domain name
-    if ((host = gethostbyname(host_name)) == NULL)
-    {
+    if ((host = gethostbyname(host_name)) == NULL){
         herror("gethostbyname");
         exit(EXIT_FAILURE);
     }
@@ -46,29 +104,23 @@ void send_mail(const char* receiver, const char* subject, const char* msg, const
 
     // TODO: Create a socket, return the file descriptor to s_fd, and establish a TCP connection to the mail server
     // Create a socket
-    if ((s_fd = socket(AF_INET, SOCK_STREAM, 0)) == -1)
-    {
-        perror("socket");
+    if ((s_fd = socket(AF_INET, SOCK_STREAM, 0)) == -1){
+        perror("socket file descriptor");
         exit(EXIT_FAILURE);
     }
 
     // Define the server address
     struct sockaddr_in server_addr;
-    memset(&server_addr, 0, sizeof(server_addr));
+
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(port);
+    server_addr.sin_addr = (struct in_addr){inet_addr(dest_ip)};
+    memset(server_addr.sin_zero, 0, sizeof(server_addr.sin_zero));
 
-    // Convert IPv4 and IPv6 addresses from text to binary form
-    if (inet_pton(AF_INET, dest_ip, &server_addr.sin_addr) <= 0)
-    {
-        perror("inet_pton");
-        close(s_fd);
-        exit(EXIT_FAILURE);
-    }
+    socklen_t addr_len = sizeof(server_addr); // addrlen 参数是套接字地址结构（sockaddr）的长度
 
     // Connect to the server
-    if (connect(s_fd, (struct sockaddr*)&server_addr, sizeof(server_addr)) == -1)
-    {
+    if (connect(s_fd, (struct sockaddr*)&server_addr, addr_len) == -1){
         perror("connect");
         close(s_fd);
         exit(EXIT_FAILURE);
@@ -76,33 +128,128 @@ void send_mail(const char* receiver, const char* subject, const char* msg, const
 
 
     // Print welcome message
-    if ((r_size = recv(s_fd, buf, MAX_SIZE, 0)) == -1)
-    {
-        perror("recv");
-        exit(EXIT_FAILURE);
-    }
-    buf[r_size] = '\0'; // Do not forget the null terminator
-    printf("%s", buf);
+    // there is a welcome message from the server using the customized recv function above
+    custom_recv(s_fd, (void *)buf, MAX_SIZE, 0, "recv");
 
     // Send EHLO command and print server response
     const char* EHLO = "EHLO 163.com\r\n"; // TODO: Enter EHLO command here
-    send(s_fd, EHLO, strlen(EHLO), 0);
-    // TODO: Print server response to EHLO command
+    custom_send(s_fd, (void *)EHLO, strlen(EHLO), 0, "send EHLO");
     
+    // TODO: Print server response to EHLO command
+    custom_recv(s_fd, (void *)buf, MAX_SIZE, 0, "recv EHLO");
+
     // TODO: Authentication. Server response should be printed out.
+    const char* AUTH_LOGIN = "AUTH login\r\n";
+    custom_send(s_fd, (void *)AUTH_LOGIN, strlen(AUTH_LOGIN), 0, "send AUTH");
+    custom_recv(s_fd, (void *)buf, MAX_SIZE, 0, "recv AUTH");
+    // send username
+    char *user_base64 = encode_str(user);
+    custom_send(s_fd, (void *)user_base64, strlen(user_base64), 0, "send user");
+    custom_recv(s_fd, (void *)buf, MAX_SIZE, 0, "recv user");
+    free(user_base64);
+    // send password
+    char *pass_base64 = encode_str(pass);
+    strcat(pass_base64, "\r\n");
+    custom_send(s_fd, (void *)pass_base64, strlen(pass_base64), 0, "send password");
+    custom_recv(s_fd, (void *)buf, MAX_SIZE, 0, "recv password");
+    free(pass_base64);
 
     // TODO: Send MAIL FROM command and print server response
-    
+    sprintf(buf, "MAIL FROM: <%s>\r\n", from);
+    custom_send(s_fd, (void *)buf, strlen(buf), 0, "send MAIL FROM");
+    custom_recv(s_fd, (void *)buf, MAX_SIZE, 0, "recv MAIL FROM");
+
     // TODO: Send RCPT TO command and print server response
-    
+    sprintf(buf, "RCPT TO: <%s>\r\n", receiver);
+    custom_send(s_fd, (void *)buf, strlen(buf), 0, "send RCPT TO");
+    custom_recv(s_fd, (void *)buf, MAX_SIZE, 0, "recv RCPT TO");
+
     // TODO: Send DATA command and print server response
+    const char* DATA = "DATA\r\n";
+    custom_send(s_fd, (void *)DATA, strlen(DATA), 0, "send DATA");
+    custom_recv(s_fd, (void *)buf, MAX_SIZE, 0, "recv DATA");
 
     // TODO: Send message data
+    // To: receiver
+    // Cc: (optional)
+    // From: sender
+    // Subject: subject
+
+    // MIME multipart set boundary string
+    const char* boundary = "----=_NextPart_000_0012_01D6D3A1.3F3A3A30";
+    // message header
+    sprintf(buf,
+            "From: %s\r\n"
+            "To: %s\r\n"
+            "Content-Type: multipart/mixed; boundary=\"%s\"\r\n",
+            from, receiver, boundary);
+    
+    if (subject != NULL){
+        strcat(buf, "Subject: ");
+        strcat(buf, subject);
+        strcat(buf, "\r\n\r\n");
+    }
+    custom_send(s_fd, (void *)buf, strlen(buf), 0, "send DATA header");
+
+    // message body
+    if (msg != NULL){
+        sprintf(
+            buf,
+            "--%s\r\n"
+            "Content-Type: text/plain; charset=\"utf-8\"\r\n"
+            "Content-Transfer-Encoding: base64\r\n\r\n",
+            boundary
+        );
+        custom_send(s_fd, (void *)buf, strlen(buf), 0, "send DATA msg");
+
+        if (access(msg, F_OK) == 0){
+            char *msg_base64 = file2str(msg);
+            custom_send(s_fd, (void *)msg_base64, strlen(msg_base64), 0, "send DATA msg file base64");
+            free(msg_base64);
+        }
+        else{
+            custom_send(s_fd, (void *)msg, strlen(msg), 0, "send DATA msg text");
+        }
+        custom_send(s_fd, "\r\n", 2, 0, "send DATA msg end");
+    }
+
+    // attachment
+    if (att_path != NULL){
+        if (gen_base64_file(att_path) == -1){
+            perror("gen_base64_file failed file not exist");
+            exit(EXIT_FAILURE);
+        }
+        else{
+            sprintf(
+                buf,
+                "--%s\r\n"
+                "Content-Type: application/octet-stream\r\n"
+                "Content-Transfer-Encoding: base64\r\n"
+                "Content-Disposition: attachment; filename=\"%s\"\r\n\r\n",
+                boundary, att_path);
+            
+            custom_send(s_fd, (void *)buf, strlen(buf), 0, "send DATA attachment header");
+
+            char *att_base64 = file2str("attach_base64");
+            custom_send(s_fd, (void *)att_base64, strlen(att_base64), 0, "send DATA attachment base64 file");
+            free(att_base64);
+        }
+
+        sprintf(buf, "--%s\r\n", boundary);
+        custom_send(s_fd, (void *)buf, strlen(buf), 0, "send DATA attachment end");
+    }
+
     
     // TODO: Message ends with a single period
+    custom_send(s_fd, (void *)end_msg, strlen(end_msg), 0, "send .");
+    custom_recv(s_fd, (void *)buf, MAX_SIZE, 0, "recv .");
 
     // TODO: Send QUIT command and print server response
+    const char* QUIT = "QUIT\r\n";
+    custom_send(s_fd, (void *)QUIT, strlen(QUIT), 0, "send QUIT");
+    custom_recv(s_fd, (void *)buf, MAX_SIZE, 0, "recv QUIT");
 
+    // Close the socket
     close(s_fd);
 }
 
